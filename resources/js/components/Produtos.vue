3bb7f92b62d55a9bd3d37f0650d6857167521a96
Teo Bandeira <timoteo.bandeira@gmail.com>
2023-05-29 17:07:14 -0300

    Correção exibição de thumbnail

--- resources/js/components/Produtos.vue
+++ resources/js/components/Produtos.vue
@@ -1,26 +1,52 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
-    import axios from 'axios';
-    const products = ref([]);
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
 
-    onMounted(() => {
-    axios.get('https://madu-experience.com.br/wp-json/wc/v3/products', {
+  const products = ref([]);
+
+  onMounted(async () => {
+    try {
+      const response = await axios.get('https://madu-experience.com.br/wp-json/wc/v3/products', {
         headers: {
-        Authorization: 'Basic ' + btoa('ck_550481d42a7c5d769b297f919d228b4e2bf76085:cs_d8d4c662b76531911a34b3918d2fcc759c71c234')
+          Authorization: 'Basic ' + btoa('ck_550481d42a7c5d769b297f919d228b4e2bf76085:cs_d8d4c662b76531911a34b3918d2fcc759c71c234')
         }
-    })
-    .then(response => {
-        products.value = response.data;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-});
+      });
+
+      products.value = response.data;
+
+      // Atualiza as URLs das imagens destacadas
+      for (const product of products.value) {
+        if (product.images && product.images.length > 0) {
+          const imageId = product.images[0].id;
+          product.imageSrc = await buscarImagemDestacada(imageId);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  async function buscarImagemDestacada(idMedia) {
+    try {
+      const response = await axios.get(`https://madu-experience.com.br/wp-json/wp/v2/media/${idMedia}`);
+
+      if (response.status === 200) {
+        const media = response.data['media_details']['sizes']['thumbnail']['source_url'];
+        return media;
+      } else {
+        throw new Error('Falha ao carregar imagem destacada');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 </script>
 
 <template>
     <div v-for="item in products" :key="item.id" class="col-md-4">
-        <img v-if="item.featured_image" :src="item.featured_image.src" :alt="item.name">
-        <a :href="'http://127.0.0.1:8000/produto/' + item.slug" class="text-decoration-none">{{ item.name }}</a>
+        <img v-if="item.imageSrc" :src="item.imageSrc" :alt="item.name" class="" />
+        <a :href="'http://madu-experience.com.br/produto/' + item.slug" class="text-decoration-none">{{ item.name }}</a>
+        <div>SKU - {{ item.sku }}</div>
+        <div>Preço - {{ item.price }}</div>
     </div>
 </template>
